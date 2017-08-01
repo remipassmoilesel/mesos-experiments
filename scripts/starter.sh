63e5049c6e0b89a5eecac90705249fc0d5e5a145
Rémi Passmoilesel <remi.passmoilesel@gmail.com>
2017-08-01 11:34:28 +0200

    Improve starter

--- scripts/starter.sh
+++ scripts/starter.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
+# Usage: wget -q -O - https://raw.githubusercontent.com/remipassmoilesel/mesos-experiments/master/scripts/starter.sh | bash
+
 apt-get update
 apt-get install -y git curl vim
 
 cd /root && git clone https://github.com/remipassmoilesel/mesos-experiments.git
+cd /root/mesos-experiments/scripts/
+chmod +x *.sh
 
-# wget -q -O - https://raw.githubusercontent.com/remipassmoilesel/mesos-experiments/master/scripts/starter.sh | bash
+
+
