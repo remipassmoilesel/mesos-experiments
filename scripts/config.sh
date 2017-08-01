11994fb6709203437c7384cc5b87298801f1fe76
Rémi Passmoilesel <remi.passmoilesel@gmail.com>
2017-08-01 10:46:47 +0200

    Improve simple scripts

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-export MASTER_ADDRESS="192.168.0.40"
+export MASTER_ADDRESS="10.0.2.25"
 export MASTER_HOST_NAME="mesos1-master1.mesos"
 
-export SLAVE1_ADDRESS="192.168.0.41"
+export SLAVE1_ADDRESS="10.0.2.27"
 export SLAVE1_HOSTNAME="mesos1-slave1.mesos"
 
-export SLAVE2_ADDRESS="192.168.0.42"
+export SLAVE2_ADDRESS="10.0.2.29"
 export SLAVE2_HOSTNAME="mesos1-slave2.mesos"
 
-export SLAVE2_ADDRESS="192.168.0.43"
-export SLAVE2_HOSTNAME="mesos1-slave3.mesos"
+export SLAVE3_ADDRESS="10.0.2.30"
+export SLAVE3_HOSTNAME="mesos1-slave3.mesos"
--- scripts/masterize.sh
+++ scripts/masterize.sh
@@ -26,20 +26,20 @@ apt-get upgrade -y
 apt-get install -y openjdk-8-jre -t jessie-backports
 
 # Fix locale for french (WIP)
-echo -e 'LANG="fr_FR.UTF-8"\nLANGUAGE="fr_FR:fr"\nLC_ALL="fr_FR"\nLC_TYPE="fr_FR.UTF-8"\n' > /etc/default/locale
-echo "fr_FR.UTF-8 UTF-8" > /etc/locale.gen
-dpkg-reconfigure -f noninteractive locales
+# echo -e 'LANG="fr_FR.UTF-8"\nLANGUAGE="fr_FR:fr"\nLC_ALL="fr_FR"\nLC_TYPE="fr_FR.UTF-8"\n' > /etc/default/locale
+# echo "fr_FR.UTF-8 UTF-8" > /etc/locale.gen
+# dpkg-reconfigure -f noninteractive locales
 
 # Add various helpers
 apt-get install -y vim byobu curl wget openssh-server ranger zsh git sed htop
 
 # Add oh my zsh for root
 cd /root && git clone https://github.com/robbyrussell/oh-my-zsh .oh-my-zsh
-cp /vagrant/common-files/zshrc /root/.zshrc
+cp common-files/zshrc /root/.zshrc
 chsh -s /bin/zsh
 
 # Change root password
-echo root:azerty | chpasswd
+# echo root:qwerty | chpasswd
 
 ##
 ## INSTALL MESOS
@@ -54,7 +54,7 @@ apt-get install -y haproxy haproxy-doc
 curl -fsSL get.docker.com | sh
 
 # Add docker group to vagrant user
-usermod -aG docker vagrant
+usermod -aG docker debian
 
 # install python pip and update it
 apt-get install -y python-pip -t jessie-backports
@@ -65,31 +65,31 @@ pip install docker-compose
 pip install mesos.cli
 
 # Add startup script (TODO: use System V)
-cp /vagrant/master-files/rc.local /etc/rc.local
+cp master-files/rc.local /etc/rc.local
 chmod +x /etc/rc.local
 
 # Enable haproxy / marathon bridge for load balancing
-/vagrant/common-files/haproxy-marathon-bridge install_haproxy_system localhost:8080
+common-files/haproxy-marathon-bridge install_haproxy_system localhost:8080
 
 # Set ip of the slave. Slave or master should choice a bad interface like loopback
 # and only have routable addresses to avoid issues
 echo "${MASTER_ADDRESS}" | sudo tee /etc/mesos-master/ip
-echo "${MASTER_HOST_NAME}" | sudo tee /etc/mesos-slave/ip
+#echo "${MASTER_HOST_NAME}" | sudo tee /etc/mesos-slave/ip
 
 # Register zookeeper master url for slave
 echo "zk://${MASTER_ADDRESS}:2181/mesos" | sudo tee /etc/mesos/zk
 
-# Register a human readable namefor cluster
-echo "Jessie-Cluster" | sudo tee /etc/mesos-master/cluster
+# Register a human readable name for cluster
+echo "Mesos1-Jessie-Cluster" | sudo tee /etc/mesos-master/cluster
 
 # Enable Mesos support for Docker
-echo "docker,mesos" | sudo tee /etc/mesos-slave/containerizers
-echo "8mins" | sudo tee /etc/mesos-slave/executor_registration_timeout
+# echo "docker,mesos" | sudo tee /etc/mesos-slave/containerizers
+# echo "8mins" | sudo tee /etc/mesos-slave/executor_registration_timeout
 
 # Add entry to /etc/hosts for slave
-echo -e "#{SLAVE1_ADDRESS} #{SLAVE1_HOSTNAME}\n" >> /etc/hosts
-echo -e "#{SLAVE2_ADDRESS} #{SLAVE2_HOSTNAME}\n" >> /etc/hosts
-echo -e "#{SLAVE3_ADDRESS} #{SLAVE3_HOSTNAME}\n" >> /etc/hosts
+echo -e "${SLAVE1_ADDRESS} #{SLAVE1_HOSTNAME}\n" >> /etc/hosts
+echo -e "${SLAVE2_ADDRESS} #{SLAVE2_HOSTNAME}\n" >> /etc/hosts
+echo -e "${SLAVE3_ADDRESS} #{SLAVE3_HOSTNAME}\n" >> /etc/hosts
 
 # First startup
 /etc/rc.local
